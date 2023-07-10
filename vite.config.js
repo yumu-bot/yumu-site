2ecb844ffd17a81e4f50c467ae61741e25d35ae3
SIyuyuko <3228981717@qq.com>
2023-07-10 15:47:33 +0800

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
           viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
           fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
           selectorBlackList: ['a-'], // 指定不转换为视窗单位的类名,例如van-（vantUI组件），
-          minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+          minPixelValue: 0, // 默认值1，小于或等于1px则不进行转换
           mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
           replace: true, // 是否转换后直接更换属性值
           // exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配，最好不要排除node_modules 文件，排除后在项目中会发现字体不能跟随页面放大
