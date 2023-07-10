d8976f06c62c1d5b5d0616b47d16f2e91f6e70ca
SIyuyuko <3228981717@qq.com>
2023-07-10 11:15:59 +0800

    调整自定义主题

--- vite.config.js
+++ vite.config.js
@@ -18,11 +18,13 @@ export default defineConfig({
       less: {
         modifyVars: {
           'primary-color': '#2A2226',//全局主色
+          // 'primary-color':'#fff',//全局主色
           'link-color': 'red',//链接色
           'border-radius-base': '2px',
           'component-background':'#46393F',//组件背景色
           // 'border-color-split':'#2A2226'//边框分割线色
-            'text-color':'#fff'//主文本色
+          'text-color': '#fff',//主文本色
+            'heading-color':'#fff'//标题色
           },
           javascriptEnabled: true,
         }, 
