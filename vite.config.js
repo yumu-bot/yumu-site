5981712dcfda35dec6f00c8a4d0908c18ba0d2e6
SIyuyuko <3228981717@qq.com>
2023-07-10 14:50:29 +0800

    增加屏幕自适应（需要后续优化）

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import postcsspxtoviewport from 'postcss-px-to-viewport'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -28,8 +27,29 @@ export default defineConfig({
           },
           javascriptEnabled: true,
         }, 
-     }
-      
+     },
+      postcss: {
+        plugins: [
+          // 屏幕自适应
+          postcsspxtoviewport({
+          unitToConvert: 'px', // 要转化的单位
+          viewportWidth: 1920, // UI设计稿的宽度
+          unitPrecision: 6, // 转换后的精度，即小数点位数
+          propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+          viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+          fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+          selectorBlackList: ['a-'], // 指定不转换为视窗单位的类名,例如van-（vantUI组件），
+          minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+          mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+          replace: true, // 是否转换后直接更换属性值
+          // exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配，最好不要排除node_modules 文件，排除后在项目中会发现字体不能跟随页面放大
+          exclude: [],
+          landscape: false // 是否处理横屏情况
+        })
+        ]
+      }
     },
   }
 )
+
+
