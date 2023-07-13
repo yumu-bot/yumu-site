8ea95977583df0495d8a2786c5e9f2b53bdb9ffc
SIyuyuko <3228981717@qq.com>
2023-07-13 10:03:37 +0800

    new file:   src/views/About.vue

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="about-page">
+		<!-- 关于内容展示 -->
+		<div class="page-content">
+			<h1>about</h1>
+		</div>
+
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped></style>
