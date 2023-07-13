1c22676a406ea87b5009dd383dc56db41850b846
SIyuyuko <3228981717@qq.com>
2023-07-13 10:01:27 +0800

    增加一级路由

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
+  <Navigation></Navigation>
   <router-view></router-view>
 </template>
 <script setup>
+import Navigation from '@/components/Navigation.vue';
 </script>
 
 <style lang="less" scoped>
