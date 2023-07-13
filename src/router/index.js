7e2f5161bc37f06af217e3f6c3c729642b5991df
SIyuyuko <3228981717@qq.com>
2023-07-13 10:01:57 +0800

    增加homecontent和about两个子路由；增加路由守卫：动态切换网页标题

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,27 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
-    component:()=>import("../views/Home.vue")
+    path: "/",
+    component: () => import("../views/Home.vue"),
+    meta: {
+      title: "yumu site | 主页"
+    },
+    children: [
+    {
+        path: "/",
+        component: () => import("../components/HomeContent.vue"),
+        meta: {
+          title: "yumu site | 主页"
+        }
+      },
+      {
+        path: "/about",
+        component: () => import("../views/About.vue"),
+        meta: {
+          title: "yumu site | 关于"
+        }
+      }
+    ]
   },
   {
 
@@ -12,6 +32,14 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // 动态切换每个页面的标题
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
 })
 
 export default router
