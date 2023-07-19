8996514962683d2edb7f1fb4cc36fe4b4a821c29
SIyuyuko <3228981717@qq.com>
2023-07-19 16:33:07 +0800

    增加路由name和默认路由重定向；增加function子路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,30 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: "/",
+    redirect:"/home",//重定向
     component: () => import("../views/Home.vue"),
     meta: {
-      title: "yumu site | 主页"
+      title: "yumu site | 主页",
     },
     children: [
     {
-        path: "/",
+        path: "/home",
+      name:"home",
         component: () => import("../components/HomeContent.vue"),
         meta: {
           title: "yumu site | 主页"
         }
+      }, {
+        path: "/function",
+        name:"function",
+        component: () => import("../views/Function/Function.vue"),
+        meta: {
+          title:"yumu site | 功能"
+        }
       },
       {
         path: "/about",
+        name:"about",
         component: () => import("../views/About.vue"),
         meta: {
           title: "yumu site | 关于"
