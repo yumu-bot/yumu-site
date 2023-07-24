9ff870b97d989ca7cccb8dc3543a63d0de45c7bf
SIyuyuko <3228981717@qq.com>
2023-07-24 18:06:14 +0800

    增加ppm查询框(静态)

--- src/views/Function/PPM.vue
+++ src/views/Function/PPM.vue
@@ -1,11 +1,41 @@
 <template>
-	ppm
+	<div class="ppm-page">
+		<div class="search-bar">
+			<a-input placeholder="请输入用户名" enter-button="点击查询" size="large" style="width: 80%;">
+			</a-input>
+			<a-select style="width: 10%;" placeholder="default" size="large">
+				<a-select-option value="osu">osu</a-select-option>
+				<a-select-option value="taiko">taiko</a-select-option>
+				<a-select-option value="catch">catch</a-select-option>
+				<a-select-option value="mania">mania</a-select-option>
+			</a-select>
+			<a-button :icon="h(SearchOutlined)" style="width: 10%;" size="large">Search</a-button>
+		</div>
+		<div class="search-result">
+			<!-- <a-spin /> -->
+		</div>
+	</div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { h } from 'vue';
+import { SearchOutlined } from '@ant-design/icons-vue';
 
-}
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.search-bar {
+	padding-top: 5px;
+	display: flex;
+	flex-direction: row;
+}
+
+.search-result {
+	text-align: center;
+	background: rgba(0, 0, 0, 0.05);
+	border-radius: 4px;
+	margin-bottom: 20px;
+	padding: 30px 50px;
+	margin: 20px 0;
+}
+</style>
