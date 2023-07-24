81952d0c4161d14a837855db82ab47d1f4ca1b88
SIyuyuko <3228981717@qq.com>
2023-07-24 15:16:59 +0800

    修改屏幕自适应插件配置

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
      },
       postcss: {
         plugins: [
-          // 屏幕自适应
+          // 屏幕自适应插件
           postcsspxtoviewport({
           unitToConvert: 'px', // 要转化的单位
           viewportWidth: 1920, // UI设计稿的宽度
@@ -40,7 +40,7 @@ export default defineConfig({
           propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
           viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
           fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
-          selectorBlackList: ['a-'], // 指定不转换为视窗单位的类名,例如van-（vantUI组件），
+          selectorBlackList: [], // 指定不转换为视窗单位的类名,例如van-（vantUI组件），
           minPixelValue: 0, // 默认值1，小于或等于1px则不进行转换
           mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
           replace: true, // 是否转换后直接更换属性值
