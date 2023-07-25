b36fbfaba86f40c4e8952d2a4cac22ed084e1bd9
SIyuyuko <3228981717@qq.com>
2023-07-25 14:33:10 +0800

    更改全局css样式类型为scss

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import antd from 'ant-design-vue'
 // import 'element-plus/dist/index.css'
 // import 'element-plus/theme-chalk/dark/css-vars.css'
 // import { createPinia } from 'pinia' // 引入状态管理
-import '@/assets/css/index.less' // 无需在此处引入全局样式，默认已经引入
+import '@/assets/css/index.scss' // 无需在此处引入全局样式，默认已经引入
 // import '@/assets/iconfont/iconfont.css' // 引入矢量图标样式
 // import '@/assets/iconfont/iconfont.js'
 import 'swiper'
