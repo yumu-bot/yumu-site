e3aa6cb044ca6d7c34ee60a0620a86f526bc5873
SIyuyuko <3228981717@qq.com>
2023-07-25 11:42:10 +0800

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.less'
+// import 'ant-design-vue/dist/antd.less'
 // import 'element-plus/dist/index.css'
 // import 'element-plus/theme-chalk/dark/css-vars.css'
 // import { createPinia } from 'pinia' // 引入状态管理
@@ -21,23 +21,3 @@ app.use(antd)
 // app.config.globalProperties.$bus = bus;
 app.mount('#app')
 
-
-
-// const debounce = (fn, delay) => {
-//   let timer = null;
-//   return function () {
-//     let context = this;
-//     let args = arguments;
-//     clearTimeout(timer);
-//     timer = setTimeout(function () {
-//       fn.apply(context, args);
-//     }, delay);
-//   }
-// }
-// const _ResizeObserver = window.ResizeObserver;
-// window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
-//   constructor(callback) {
-//     callback = debounce(callback, 16);
-//     super(callback);
-//   }
-// }
