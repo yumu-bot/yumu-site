9aa71b4d89e524d55814d6b5a092a6afb9bbfb45
SIyuyuko <3228981717@qq.com>
2023-07-25 15:27:57 +0800

    modified:   src/views/Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,9 @@ import { onMounted } from 'vue';
 // onMounted(()=>{windowResize();});
 </script>
 
-<style lang="less" scoped>
+<style lang="scss" scoped>
+@import "../assets/css/variables.scss";
+
 .home-page {
 	// min-width: 1024px;
 	// min-height: 960px;
@@ -29,6 +31,11 @@ import { onMounted } from 'vue';
 	// overflow: hidden;
 	// z-index: auto;
 	color: #ffffff;
+
+	@media screen and (min-width: 1921px) {
+		width: 90%;
+		margin: 0 auto;
+	}
 }
 
 .page-content {
@@ -48,66 +55,34 @@ import { onMounted } from 'vue';
 	z-index: 1;
 	margin: 0 auto; //高度自适应实现垂直居中,父级元素设置display: flex;
 	padding-top: 30px;
+
 	// display: inline-block;
-}
-
-@media screen and (max-width: 480px) {
-	.page-content {
-		width: 100%;
-		padding-top: 24.208333vw;
+	@media screen and (min-width:$xxl) and (max-width:1919px) {
+		width: 66%;
 	}
-}
 
-@media screen and (min-width: 481px) and (max-width: 640px) {
-	.page-content {
-		width: 100%;
-		padding-top: 120px;
+	@media screen and (max-width:$xxl) {
+		width: 67%;
 	}
-}
 
-@media screen and (min-width: 641px) and (max-width: 768px) {
-	.page-content {
-		width: 86%;
-		padding-top: 90px;
+	@media screen and (max-width:$xl) {
+		width: 70%;
 	}
-}
 
-@media screen and (min-width: 769px) and (max-width: 960px) {
-	.page-content {
-		width: 77%;
-		padding-top: 60px;
-	}
-}
-
-@media screen and (min-width: 961px) and (max-width: 1098px) {
-	.page-content {
+	@media screen and (max-width:$lg) {
 		width: 73%;
-		padding-top: 50px;
 	}
-}
 
-@media screen and (min-width: 1099px) and (max-width:1280px) {
-	.page-content {
-		width: 69%;
-		padding-top: 40px;
+	@media screen and (max-width:$md) {
+		width: 85%;
 	}
-}
 
-@media screen and (min-width: 1281px) and (max-width:1536px) {
-	.page-content {
-		width: 64%;
+	@media screen and (max-width:$sm) {
+		width: 100%;
 	}
-}
 
-@media screen and (min-width: 1537px) and (max-width:1919px) {
-	.page-content {
-		width: 61%;
-	}
-}
-
-@media screen and (min-width: 1921px) {
-	.page-content {
-		width: 50%;
+	@media screen and (max-width:$xs) {
+		width: 100%;
 	}
 }
 </style>
