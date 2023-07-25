f38594735c255baffbbc9cd47664015816048f8c
SIyuyuko <3228981717@qq.com>
2023-07-25 11:43:01 +0800

    new file:   src/components/Theme/Theme.vue

---
+++ src/components/Theme/Theme.vue
@@ -0,0 +1,18 @@
+<template>
+	<!-- Ant Design v4 全局自定义主题 -->
+	<a-config-provider :theme="{
+		token: {
+			colorPrimary: '#2a2226',
+			colorInfo: '#2a2226',
+			fontSize: 14,
+			wireframe: true
+		},
+	}">
+	</a-config-provider>
+</template>
+
+<script setup>
+import { theme } from 'ant-design-vue';
+</script>
+
+<style></style>
