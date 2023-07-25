a5240a480022e983b5bc214fe03abe83d1c7ba35
SIyuyuko <3228981717@qq.com>
2023-07-25 11:41:39 +0800

    引入全局自定义主题组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <Theme></Theme>
   <Navigation></Navigation>
   <router-view></router-view>
 </template>
 <script setup>
 import Navigation from '@/components/Navigation.vue';
+import Theme from './components/Theme/Theme.vue';
 </script>
 
 <style lang="less" scoped></style>
