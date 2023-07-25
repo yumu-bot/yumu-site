746837290b42d39549f641a35cc1922cd534dd2b
SIyuyuko <3228981717@qq.com>
2023-07-25 16:07:27 +0800

    modified:   src/components/Theme/Theme.vue

--- src/components/Theme/Theme.vue
+++ src/components/Theme/Theme.vue
@@ -3,16 +3,38 @@
 	<a-config-provider :theme="{
 		token: {
 			colorPrimary: '#2a2226',
-			colorInfo: '#2a2226',
+			// colorInfo: '#2a2226',
 			fontSize: 14,
-			wireframe: true
+			wireframe: true,
+			borderRadius: 0,
+			// colorBgContainer: '#2A2226',
+			// colorText: '#ffffff',
+			// colorItemText:'#ffffff',
 		},
+		components: {
+			Input: {
+				// colorBgContainer: '#2A2226',
+				// colorTextPlaceholder: 'rgba(255,255,255,0.45)'
+			},
+			Select: {
+				// colorText: 'rgba(42,34,38,0.5)',
+				// colorTextPlaceholder: '#ffffff',
+				// colorPrimaryHover: '#ffffff',
+				// controlItemBgActive: '#ffffff',
+				// colorTextQuaternary: '#ffffff'
+			},
+			Menu: {
+				// borderRadius:16
+				// colorItemBg: '#2A2226',
+				// colorText: '#ffffff',
+			}
+		}
 	}">
 	</a-config-provider>
 </template>
 
 <script setup>
-import { theme } from 'ant-design-vue';
+import { Menu, Select, theme } from 'ant-design-vue';
 </script>
 
 <style></style>
