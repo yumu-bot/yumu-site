d1c84c0ccc8e96c4ca2198e93112416f27335c97
SIyuyuko <3228981717@qq.com>
2023-08-14 10:24:01 +0800

    modified:   src/components/Theme/Theme.vue

--- src/components/Theme/Theme.vue
+++ src/components/Theme/Theme.vue
@@ -1,21 +1,26 @@
 <template>
 	<!-- Ant Design v4 全局自定义主题 -->
+	<!-- 在线主题编辑器：https://antdv.com/theme-editor-cn  -->
 	<a-config-provider :theme="{
+		// 全局配置
 		token: {
-			colorPrimary: '#2a2226',
+			colorPrimary: '#2a2226',//主色
 			// colorInfo: '#2a2226',
-			fontSize: 14,
+			fontSize: 14,//默认字号
 			wireframe: true,
-			borderRadius: 14,
+			borderRadius: 14,//基础圆角
+			// colorPrimaryText:'fff',//主色文本
 			// colorBgContainer: '#2A2226',
 			// colorText: '#ffffff',
 			// colorItemText:'#ffffff',
 		},
 		components: {
+			// 输入框
 			Input: {
 				// colorBgContainer: '#2A2226',
 				// colorTextPlaceholder: 'rgba(255,255,255,0.45)'
 			},
+			// 选择框
 			Select: {
 				colorText: '#ffffff',
 				// colorPrimary: '#2a2226',
@@ -29,12 +34,14 @@
 				controlItemBgActive: 'rgba(255,255,255,0.65)',
 				colorBgElevated: '#46393F'
 			},
+			// 目录菜单
 			Menu: {
 				// borderRadius:16
 				colorItemBg: '#54454C',
 				colorPrimary: '#ffffff',
 				colorItemText: '#ffffff'
 			},
+			// 按钮
 			Button: {
 				colorBgContainer: '#2A2226',
 				colorText: '#ffffff'
