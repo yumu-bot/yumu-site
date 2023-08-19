5f427924749ae35cef952f39c6f623c641bc5060
SIyuyuko <3228981717@qq.com>
2023-08-19 14:56:00 +0800

    新增玩家对比&比赛查询&谱面查询&娱乐功能页

---
+++ src/views/Function/Beatmap.vue
@@ -0,0 +1,11 @@
+<template>
+	beatmap
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
---
+++ src/views/Function/Entertainment.vue
@@ -0,0 +1,11 @@
+<template>
+	entertainment
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
